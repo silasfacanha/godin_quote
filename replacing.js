4830f28b31c67448c22589d8a7281b4ee92b11d5
silasfacanha <silasfacanha@hotmail.com>
2022-09-06 13:16:41 -0300

    I've adjusted the button that didn't look like a button

---
+++ replacing.js
@@ -0,0 +1,300 @@
+const culture_quotes = [
+  {
+    quote:
+      "The status quo doesn’t shift because you’re right. It shifts because the culture changes. And the engine of culture is status.",
+    source: "This is Marketing",
+    year: 2018,
+  },
+  {
+    quote: "People like us do things like this: this is culture.",
+    source: "Seth's Blog",
+    year: 2013,
+  },
+
+  {
+    quote:
+      "The first is that the easiest thing to do is merely amplify what a culture is already embracing. The second is that real change is cultural change, and you must go about it with the intent to change the culture, not to merely make the easy change, the easy sale.",
+    source: "Seth's Blog",
+    year: 2013,
+  },
+
+  {
+    quote:
+      "The Bhagavad-Gita says, “It is better to follow your own path, however imperfectly, than to follow someone else’s perfectly.",
+    source: "The Practice",
+    year: 2020,
+  },
+
+  {
+    quote:
+      "Askıda ekmek: there is bread on the hook. It’s an ancient tradition in Turkey. When buying a loaf at the local bakery, you can choose to pay for an extra loaf and, after bagging your purchase, the owner will hang the second loaf on a hook on the wall. If a person in need comes by, he or she can ask if there’s anything on the hook. If so, the bread is shared, and the hunger is relieved.",
+    source: "The Practice",
+    year: 2020,
+  },
+
+  {
+    quote:
+      "The essence of political change is almost always cultural change, and the culture changes horizontally. Person to person. Us to us.",
+    source: "This is Marketing",
+    year: 2018,
+  },
+
+  {
+    quote:
+      "The practice seeks to make change, but the process demands originality. The practice is consistent, but only in intention, not in execution.",
+    source: "The Practice",
+    year: 2020,
+  },
+
+  {
+    quote:
+      "That's your opportunity--to approach your work in a way that generates unique learning and interactions that are worth sharing.",
+    source: "Poke The Box",
+    year: 2011,
+  },
+];
+
+const marketing_quotes = [
+  {
+    quote:
+      "Marketing, more than a lake or a forest, is the landscape of our modern lives.",
+    source: "This is Marketing",
+    year: 2018,
+  },
+  {
+    quote:
+      "Tiffany’s blue box is a slogan without words. It stands for elegance and packaging and quality and 'price is no object'",
+    source: "Purple Cow",
+    year: 2002,
+  },
+
+  {
+    quote:
+      "The obvious winners are the mid-sized and smaller companies looking to increase market share. These are the companies that have nothing to lose, but more important, they realize that they have a lot to gain by changing the rules of the game.",
+    source: "Purple Cow",
+    year: 2002,
+  },
+
+  {
+    quote:
+      "The best marketers are farmers, not hunters. Plant, tend, plow, fertilize, weed, repeat. Let someone else race around after shiny objects.",
+    source: "This is Marketing",
+    year: 2018,
+  },
+
+  {
+    quote:
+      "Low price is the last refuge of a marketer who has run out of generous ideas.",
+    source: "This is Marketing ",
+    year: 2018,
+  },
+
+  {
+    quote:
+      "Leaders lead when they take positions, when they connect with their tribes, and when they help the tribe connect to itself.",
+    source: "Tribes",
+    year: 2008,
+  },
+
+  {
+    quote:
+      "If your organization requires success before commitment, it will never have either. Part of leadership (a big part of it, actually) is the ability to stick with the dream for a long time. Long enough that the critics realize that you're going to get there one way or another...so they follow.",
+    source: "Tribes",
+    year: 2008,
+  },
+
+  {
+    quote:
+      "Persistent, consistent, and frequent stories, delivered to an aligned audience, will earn attention, trust, and action.",
+    source: "This is Marketing",
+    year: 2018,
+  },
+];
+const work_quotes = [
+  {
+    quote:
+      "Instead of wondering when your next vacation is, maybe you should set up a life you don't need to escape from.",
+    source: "Purple Cow: Transform your Business by Being Remarkable",
+    year: 2002,
+  },
+  {
+    quote:
+      "If you do a job where someone tells you exactly what to do, he will find someone cheaper than you to do it.",
+    source: "Small is the New Big",
+    year: 2007,
+  },
+
+  {
+    quote:
+      "In your career, even more than for a brand, being safe is risky. The path to lifetime job security is to be remarkable.",
+    source: "Purple Cow",
+    year: 2002,
+  },
+
+  {
+    quote:
+      "Writers write. Runners run. Establish your identity by doing your work.",
+    source: "The Practice",
+    year: 2020,
+  },
+
+  {
+    quote: "Your work is too important to be left to how you feel today.",
+    source: "The Practice",
+    year: 2020,
+  },
+
+  {
+    quote:
+      "You are not your work. Your work is a series of choices made with generous intent to cause something to happen. We can always learn to make better choices.",
+    source: "The Practice",
+    year: 2020,
+  },
+
+  {
+    quote:
+      "Today, not starting is far, far worse than being wrong. If you start, you've got a shot at evolving and adjusting to turn your wrong into a right. But if you don't start, you never get a chance.",
+    source: "Poke The Box",
+    year: 2011,
+  },
+
+  {
+    quote:
+      "Everything gets easier when you walk away from the hubris of everyone. Your work is not for everyone. It’s only for those who signed up for the journey.",
+    source: "This is Marketing",
+    year: 2018,
+  },
+];
+const ideas_quotes = [
+  {
+    quote:
+      "If you can’t state your position in eight words, you don’t have a position.",
+    source: "Purple Cow",
+    year: 2002,
+  },
+  {
+    quote: "The opposite of remarkable is very good.",
+    source: "Purple Cow: Transform your Business by Being Remarkable",
+    year: 2002,
+  },
+
+  {
+    quote:
+      "Nobody says 'Yeah, I'd like to set myself up for some serious criticism!' And yet the only way to be remarkable is to do just that.",
+    source: "Purple Cow: Transform your Business by Being Remarkable",
+    year: 2002,
+  },
+
+  {
+    quote: "Do what you should do. Your motivation will follow.",
+    source: "What to do When it's Your Turn (and it's Always Your Turn)",
+    year: 2014,
+  },
+
+  {
+    quote: "The person who fails the most wins.",
+    source: "What to do When it's Your Turn (and it's Always Your Turn)",
+    year: 2014,
+  },
+
+  {
+    quote:
+      "The devil doesn't need an advocate. The brave need supporters, not critics.",
+    source: "Whatcha Going to Do with That Duck?",
+    year: 2012,
+  },
+
+  {
+    quote: "Who's got your back? More important, whose back do you have?",
+    source: "Whatcha Going to Do with That Duck?",
+    year: 2012,
+  },
+
+  {
+    quote: "People who make up new rules continue to be in very short supply.",
+    source: "Small is the New Big",
+    year: 2007,
+  },
+];
+
+const life_quotes = [
+  {
+    quote:
+      "You do not equal the project. Criticism of the project is not criticism of you.",
+    source: "Purple Cow",
+    year: 2002,
+  },
+  {
+    quote:
+      "We run our schools like factories. We line kids up in straight rows, put them in batches (called grades), and work very hard to make sure there are no defective parts. Nobody standing out, falling behind, running ahead, making a ruckus. Playing it safe. Following the rules. Those seem like the best ways to avoid failure.",
+    source: "Purple Cow",
+    year: 2002,
+  },
+
+  {
+    quote:
+      "The only choice we have is to begin. And the only place to begin is where we are. Simply begin. But begin.",
+    source: "The Practice",
+    year: 2020,
+  },
+
+  {
+    quote:
+      "Hard work is about risk. It begins when you deal with the things that you would rather not deal with: fear of failure, fear of standing out, fear of rejection. Hard work is about training yourself to leap over this barrier, drive through the other barrier. And after you have done that, to do it again the next day.",
+    source: "Small is the New Big",
+    year: 2007,
+  },
+
+  {
+    quote:
+      "The relentless pursuit of mass will make you boring, because mass means average, it means the center of the curve, it requires you to offend no one and satisfy everyone.",
+    source: "This is Marketing",
+    year: 2018,
+  },
+
+  {
+    quote: "Anxiety is experiencing failure in advance.",
+    source: "Poke the Box",
+    year: 2011,
+  },
+
+  {
+    quote: "Soon is not as good as now.",
+    source: "Poke the Box ",
+    year: 2011,
+  },
+
+  {
+    quote:
+      "All great programmers learn the same way. They poke the box. They code something and see what the computer does. They change it and see what the computer does. They repeat the process again and again until they figure out how the box works.",
+    source: "Poke the Box",
+    year: 2011,
+  },
+];
+
+culture_quotes.forEach((object) => {
+  object.label = "culture";
+});
+
+marketing_quotes.forEach((object) => {
+  object.label = "marketing";
+});
+work_quotes.forEach((object) => {
+  object.label = "work";
+});
+ideas_quotes.forEach((object) => {
+  object.label = "ideas";
+});
+life_quotes.forEach((object) => {
+  object["label"] = "life";
+});
+let quotes = culture_quotes.concat(
+  marketing_quotes,
+  work_quotes,
+  ideas_quotes,
+  life_quotes
+);
+
+let formattedQuotes = JSON.stringify(quotes);
+
+console.log(formattedQuotes);
